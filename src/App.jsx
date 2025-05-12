39f81d687cc4a13733a0d11f1428cdd204b0fb2b
Julia Siger <246948@edu.p.lodz.pl>
2025-05-12 15:49:43 +0200

    learning:testing useEffect and conditionals

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 
 const Card = ({title, rating}) => {
+  const [hasLiked, setHasLiked] = useState(false);
+  useEffect(() => {
+    console.log(`${title} has been ${hasLiked}`)
+  }, [hasLiked]);
   return (
-    <div>
+    <div className = "card">
       <h2>{title}</h2>
-      <h2>I rate it:{rating}</h2>
+      <button onClick={()=> setHasLiked(!hasLiked)}>
+        {hasLiked ? "Liked!" : "Like"}
+      </button>
     </div>
   );
 };
 
 const App = () => {
   return (
-    <div>
-      <h2>Functional arrow component</h2>
-      <Card title="My year of rest and relaxation" rating="5" />
+    <div className='card-container'>
+      <Card title="My year of rest and relaxation" rating="5"/>
       <Card title="Lolita" rating = "3"/>
       <Card title="The old man and the sea" rating ="4"/>
-      <Card />
     </div>
 
   );
