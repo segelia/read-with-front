05fe0347c46e0648966f39fd6b0f0c9b0da11605
Julia Siger <246948@edu.p.lodz.pl>
2025-05-12 13:50:21 +0200

    feat: changed the App component and added the Card component

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const Card = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <h2>Functional CARD component</h2>
+
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <h2>Functional arrow component</h2>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+
+  );
+};
+
 
 export default App
